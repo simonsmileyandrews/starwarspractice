6228c4383a8be4eb2af291adc678df570b5fbbee
Simon Smiley-Andrews <simonsmiley-andrews@Outwares-MacBook-Pro.local>
2017-11-21 14:36:41 +1100

    Initial Commit

---
+++ StarWarsPracticeTests/StarWarsPracticeTests.swift
@@ -0,0 +1,36 @@
+//
+//  StarWarsPracticeTests.swift
+//  StarWarsPracticeTests
+//
+//  Created by Simon Smiley-Andrews on 21/11/17.
+//  Copyright © 2017 Simon Smiley-Andrews. All rights reserved.
+//
+
+import XCTest
+@testable import StarWarsPractice
+
+class StarWarsPracticeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
